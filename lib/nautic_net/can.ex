c1743d6e144b0d34668941276dd11f6ef27cdc40
Rockwell Schrock <rockwell@schrock.me>
2023-06-08 12:21:47 -0400

    Add CAN.transmit_packet/1

--- lib/nautic_net/can.ex
+++ lib/nautic_net/can.ex
@@ -3,6 +3,8 @@ defmodule NauticNet.CAN do
   Entrypoint for reading and writing from the CAN bus.
   """
 
+  alias NauticNet.NMEA2000.Packet
+
   def child_spec(config) do
     %{
       id: __MODULE__,
@@ -11,4 +13,12 @@ defmodule NauticNet.CAN do
   end
 
   defdelegate transmit_frame(frame), to: NauticNet.CAN.Server
+
+  def transmit_packet(%Packet{} = packet) do
+    for frame <- Packet.to_frames(packet) do
+      transmit_frame(frame)
+    end
+
+    :ok
+  end
 end
